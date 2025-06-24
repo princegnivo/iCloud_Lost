fcf9b72b751f74c11601e01a6d4b486bf447965e
PrinceMxMx <princegnivo0@gmail.com>
2025-06-24 18:58:58 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,26 +2,31 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-between lg:flex-row text-xs w-full bg-customBG-darkLess text-gray-200 lg:px-44 py-6 mt-auto">
-      <div className="flex">
-        <button className="p-2">
-          <a href="https://www.apple.com/support/systemstatus/">
-            System Status
-          </a>
-        </button>
-        <div className="p-2">|</div>
-        <button className="p-2">
-          <a href="https://www.apple.com/legal/privacy/">Privacy Policy</a>
-        </button>
-        <div className="p-2">|</div>
-        <button className="p-2">
-          <a href="https://www.apple.com/legal/internet-services/icloud/">
-            Terms & Conditions
-          </a>
-        </button>
+    <footer className="flex flex-col items-center justify-between lg:flex-row text-xs w-full bg-white text-black lg:px-44 py-6 mt-auto border-t border-gray-200">
+      <div className="flex flex-wrap justify-center lg:justify-start">
+        <a
+          href="https://www.apple.com/support/systemstatus/"
+          className="p-2 hover:underline"
+        >
+          System Status
+        </a>
+        <span className="p-2">|</span>
+        <a
+          href="https://www.apple.com/legal/privacy/"
+          className="p-2 hover:underline"
+        >
+          Privacy Policy
+        </a>
+        <span className="p-2">|</span>
+        <a
+          href="https://www.apple.com/legal/internet-services/icloud/"
+          className="p-2 hover:underline"
+        >
+          Terms & Conditions
+        </a>
       </div>
-      <div className="flex">
-        Copyright © 2025 Apple Inc. All rights reserved.
+      <div className="flex mt-4 lg:mt-0">
+        © 2025 Apple Inc. All rights reserved.
       </div>
     </footer>
   );
