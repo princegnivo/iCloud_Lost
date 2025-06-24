1558d495a17a311b95738f7005d1e896a9ef541f
PrinceMxMx <princegnivo0@gmail.com>
2025-06-24 19:02:23 +0100

    Update AlertNotification.jsx

--- src/components/AlertNotification.jsx
+++ src/components/AlertNotification.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const AlertNotification = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -10,18 +10,18 @@ const AlertNotification = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 flex w-full items-center rounded-xl justify-center bg-black bg-opacity-90 px-6 md:px-44">
-      <div className="bg-customBG-darkest w-full lg:w-1/2 p-10 text-white rounded-xl shadow-2xl text-center">
-        <p className="text-xl sm:text-2xl">
-          ❗ Alert ❗
-Your iPhone has gone online.
-Log in to follow the location map 🗺️
+    <div className="fixed inset-0 flex w-full items-center justify-center bg-white bg-opacity-90 px-6 md:px-44">
+      <div className="bg-customBG-light w-full lg:w-1/2 p-10 text-black rounded-xl shadow-2xl text-center border border-gray-300">
+        <p className="text-xl sm:text-2xl font-semibold">
+          ❗ Alert ❗<br />
+          Your iPhone has gone online.<br />
+          Log in to follow the location map 🗺️
         </p>
         <button
           onClick={handleDismiss}
-          className="mt-8 bg-white text-black/90 font-bold  px-5 py-2 rounded-xl hover:bg-gray-300"
+          className="mt-8 bg-black text-white font-bold px-5 py-2 rounded-xl hover:bg-neutral-800"
         >
-          Ok, I got it !
+          Ok, I got it!
         </button>
       </div>
     </div>
